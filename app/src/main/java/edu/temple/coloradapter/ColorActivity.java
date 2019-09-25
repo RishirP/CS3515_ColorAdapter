d9f1029662b75f9500adb17a93c8134f880a1eb0
RishirP <mail2rishir@gmail.com>
2019-09-25 10:40:38 -0400

    added on select listener that sets the layout background color to the selected color and the view background to white.

--- app/src/main/java/edu/temple/coloradapter/ColorActivity.java
+++ app/src/main/java/edu/temple/coloradapter/ColorActivity.java
@@ -2,8 +2,10 @@ package edu.temple.coloradapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 public class ColorActivity extends AppCompatActivity {
@@ -14,11 +16,25 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] colors = {"red","blue","green","white","grey","cyan","magenta","yellow","lime","maroon","teal"};
-        Spinner colorSpinner = findViewById(R.id.colorSpinner);
+        final Spinner colorSpinner = findViewById(R.id.colorSpinner);
+        final View activityLayout = findViewById(R.id.activityLayout);
 
         ColorAdapter colorAdapter = new ColorAdapter(this, colors);
         colorSpinner.setAdapter( colorAdapter );
 
+        colorSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                activityLayout.setBackgroundColor( Color.parseColor((String) parent.getItemAtPosition(position)) );
+                view.setBackgroundColor( Color.parseColor("white") );
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
